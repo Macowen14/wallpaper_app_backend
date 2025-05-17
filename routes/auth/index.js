6fd2e7cb2ac9460b67e0b98c8dd05ab7981ddabc
Macowen14 <mwingamacowen@outlook.com>
2025-05-17 13:56:58 +0300

    route update

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 import {register, login} from "../../controllers/authController.js"
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/auth/register', register);
+router.post('/auth/login', login);
 
 export default router;
